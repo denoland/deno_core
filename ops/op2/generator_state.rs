6bd17356f81499644a7e4b2b1fc6faa57a075765
Matt Mastracci <matthew@mastracci.com>
2023-07-19 14:08:53 -0600

    test fix

--- ops/op2/generator_state.rs
+++ ops/op2/generator_state.rs
@@ -49,7 +49,7 @@ pub struct GeneratorState {
 /// Example: this will extract `deno_core`, `info` and `scope` from `generator_state`
 /// before invoking the [`quote!`] macro.
 ///
-/// ```
+/// ```nocompile
 ///  gs_quote!(generator_state(deno_core, info, scope) =>
 ///    (let mut #scope = unsafe { #deno_core::v8::CallbackScope::new(&*#info) };)
 ///  )
@@ -71,7 +71,7 @@ macro_rules! gs_quote {
 ///
 /// Extracts `deno_core` from `generator_state` into a local variable. Equivalent to `let deno_core = &generator_state.deno_core`.
 ///
-/// ```
+/// ```nocompile
 /// gs_extract!(generator_state(deno_core))
 /// ```
 macro_rules! gs_extract {
