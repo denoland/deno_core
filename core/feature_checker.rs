50b4a8760b3765226841164d3540c015f2c6c2d2
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-10-02 22:42:38 +0200

    David's review

--- core/feature_checker.rs
+++ core/feature_checker.rs
@@ -1,26 +1,25 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
-use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 
 #[derive(Default, Debug)]
 pub struct FeatureChecker {
-  features: BTreeMap<&'static str, bool>,
+  features: BTreeSet<&'static str>,
   // TODO(bartlomieju): remove once we migrate away from `--unstable` flag
   // in the CLI.
   legacy_unstable: bool,
 }
 
 impl FeatureChecker {
-  pub fn update(&mut self, feature: &'static str, enabled: bool) {
-    let prev = self.features.insert(feature, enabled);
-    assert!(prev.is_none());
+  pub fn enable_feature(&mut self, feature: &'static str) {
+    self.features.insert(feature);
   }
 
   /// Check if a feature is enabled.
   ///
   /// If a feature in not present in the checker, return false.
   pub fn check(&self, feature: &str) -> bool {
-    *self.features.get(feature).unwrap_or(&false)
+    self.features.contains(feature)
   }
 
   // TODO(bartlomieju): remove this.
@@ -31,7 +30,7 @@ impl FeatureChecker {
   // TODO(bartlomieju): remove this.
   /// Check if `--unstable` flag has been passed. If not then exit the process
   /// with exit code 70.
-  pub fn check_legacy_unstable(&self, api_name: &str) {
+  pub fn check_legacy_unstable_or_exit(&self, api_name: &str) {
     if !self.legacy_unstable {
       eprintln!(
         "Unstable API '{api_name}'. The --unstable flag must be provided."
