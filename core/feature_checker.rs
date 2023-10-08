3eb353904874e7db5b0da7167b09cbd5e97dc358
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-10-09 00:06:32 +0200

    refactor: updates to FeatureChecker

--- core/feature_checker.rs
+++ core/feature_checker.rs
@@ -2,29 +2,71 @@
 
 use std::collections::BTreeSet;
 
+fn exit(feature: &str, api_name: &str) {
+  eprintln!("Unstable API '{api_name}'. The `--unstable-{feature}` flag must be provided.");
+  std::process::exit(70);
+}
+
+fn warn_legacy_flag(feature: &str) {
+  eprintln!("The `--unstable` flag is deprecated, use `--unstable-{feature}` instead.");
+}
+
 #[derive(Default, Debug)]
 pub struct FeatureChecker {
   features: BTreeSet<&'static str>,
   // TODO(bartlomieju): remove once we migrate away from `--unstable` flag
   // in the CLI.
   legacy_unstable: bool,
+  warn_on_legacy_unstable: bool,
 }
 
 impl FeatureChecker {
   pub fn enable_feature(&mut self, feature: &'static str) {
-    self.features.insert(feature);
+    let inserted = self.features.insert(feature);
+    assert!(inserted, "Trying to enabled a feature that is already enabled {}", feature);
   }
 
   /// Check if a feature is enabled.
   ///
   /// If a feature in not present in the checker, return false.
+  #[inline(always)]
   pub fn check(&self, feature: &str) -> bool {
     self.features.contains(feature)
+  }
+
+  #[inline(always)]
+  pub fn check_or_exit(&self, feature: &str, api_name: &str) {
+    if !self.check(feature) {
+      exit(feature, api_name);
+    }
+  }
+
+  #[inline(always)]
+  pub fn check_or_exit_with_legacy_fallback(
+    &self,
+    feature: &str,
+    api_name: &str,
+  ) {
+    if !self.features.contains(feature) {
+      if self.legacy_unstable {
+        if self.warn_on_legacy_unstable {
+          warn_legacy_flag(feature);
+        }
+        return;
+      }
+
+      exit(feature, api_name);
+    }
   }
 
   // TODO(bartlomieju): remove this.
   pub fn enable_legacy_unstable(&mut self) {
     self.legacy_unstable = true;
+  }
+
+  // TODO(bartlomieju): remove this.
+  pub fn warn_on_legacy_unstable(&mut self) {
+    self.warn_on_legacy_unstable = true;
   }
 
   // TODO(bartlomieju): remove this.
