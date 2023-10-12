541b3bee637a1852af8764bea2079b8f816255d5
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-10-12 14:41:02 +0200

    Revert "perf: Use V8 Deno builtins (#261)" (#265)
    This reverts commit b37184737dd113eaf3df46b47505e14a2fc9cbb7.

--- core/runtime/jsruntime.rs
+++ core/runtime/jsruntime.rs
@@ -342,7 +342,6 @@ fn v8_init(
     " --no-validate-asm",
     " --turbo_fast_api_calls",
     " --harmony-change-array-by-copy",
-    " --expose_deno_builtins",
   );
   let predictable_flags = "--predictable --random-seed=42";
   let expose_natives_flags = "--expose_gc --allow_natives_syntax";
