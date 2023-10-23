6755efc22c3e3e6d34635100e0ad5413c9e451d7
Matt Mastracci <matthew@mastracci.com>
2023-10-23 12:43:25 +0900

    add compile test for async_deferred

---
+++ ops/op2/test_cases/async/async_deferred.rs
@@ -0,0 +1,8 @@
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+#![deny(warnings)]
+deno_ops_compile_test_runner::prelude!();
+
+#[op2(async(deferred), fast)]
+pub async fn op_async_deferred() -> std::io::Result<i32> {
+    Ok(0)
+}
