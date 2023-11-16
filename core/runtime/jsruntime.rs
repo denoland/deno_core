ae3bfcf208c12a139384bca2b8884711a5734ca8
Jakub Jirutka <jakub@jirutka.cz>
2023-11-17 00:51:50 +0100

    build(core): add feature flag include_icu_data (#330)
    Embedding ICU data into Deno binary is undesirable when linking with
    system-provided ICU (in Linux distributions) - it adds 10 MiB of
    unnecessary bloat.
    
    This patch is already used in deno package for Alpine Linux.

--- core/runtime/jsruntime.rs
+++ core/runtime/jsruntime.rs
@@ -345,11 +345,14 @@ fn v8_init(
   predictable: bool,
   expose_natives: bool,
 ) {
-  // Include 10MB ICU data file.
-  #[repr(C, align(16))]
-  struct IcuData([u8; 10631872]);
-  static ICU_DATA: IcuData = IcuData(*include_bytes!("icudtl.dat"));
-  v8::icu::set_common_data_73(&ICU_DATA.0).unwrap();
+  #[cfg(feature = "include_icu_data")]
+  {
+    // Include 10MB ICU data file.
+    #[repr(C, align(16))]
+    struct IcuData([u8; 10631872]);
+    static ICU_DATA: IcuData = IcuData(*include_bytes!("icudtl.dat"));
+    v8::icu::set_common_data_73(&ICU_DATA.0).unwrap();
+  }
 
   let base_flags = concat!(
     " --wasm-test-streaming",
