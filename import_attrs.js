5c1af80011e015e26e7bbd699cbb21c273632b3c
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-11-27 23:14:34 +0100

    remove some files

--- import_attrs.js
+++ import_attrs.js
@@ -1,7 +1,5 @@
+// deno-lint-ignore-file
+
 import text from "./log.txt" with { type: "text" };
-import styles from "./styles.css" with { type: "css-module" };
 
-// Deno.core.print(`text ${text}\n\n`);
-
-Deno.core.print(`styles .foo ${styles.foo}\n`);
-Deno.core.print(`styles .bar ${styles.bar}\n`);
+Deno.core.print(`text ${text}\n\n`);
