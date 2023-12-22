a812b54c5e33068508479938f32845180067cbf4
Matt Mastracci <matthew@mastracci.com>
2023-12-22 10:28:31 -0700

    copyright

--- core/runtime/op_driver/mod.rs
+++ core/runtime/op_driver/mod.rs
@@ -1,6 +1,6 @@
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 use crate::OpId;
 use crate::PromiseId;
-// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 use crate::ops::OpCtx;
 use anyhow::Error;
 use std::future::Future;
