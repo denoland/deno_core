0d0e362a10f86cc56f6ee53069c2044b6abbb316
Kenta Moriuchi <moriken@kimamass.com>
2023-12-25 04:20:24 +0900

    fix for lint

--- core/lib.deno_core.d.ts
+++ core/lib.deno_core.d.ts
@@ -1,6 +1,6 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 
-// deno-lint-ignore-file no-explicit-any
+// deno-lint-ignore-file ban-types no-explicit-any
 
 /// <reference no-default-lib="true" />
 /// <reference lib="esnext" />
