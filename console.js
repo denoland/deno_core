89188024b7b5abe4547459e420ec30c00a121064
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-12-27 23:01:26 +0100

    remove unneeded file

--- console.js
+++
@@ -1,8 +0,0 @@
-const obj = {
-    a: 1,
-    b: true,
-    c: [1, 2, 3]
-}
-console.log("Hello there", 1, true, new Date(), obj);
-console.warn("Hello there", 1, true, new Date(), obj);
-console.error("Hello there", 1, true, new Date(), obj);
