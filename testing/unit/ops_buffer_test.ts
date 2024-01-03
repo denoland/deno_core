04ca545efbc26f75d3a57a5ad86d7bbc9b93bd4b
Matt Mastracci <matthew@mastracci.com>
2024-01-02 17:31:40 -0700

    fmt

--- testing/unit/ops_buffer_test.ts
+++ testing/unit/ops_buffer_test.ts
@@ -2,22 +2,22 @@
 import { assertArrayEquals, test } from "checkin:testing";
 const {
   op_v8slice_store,
-  op_v8slice_clone
+  op_v8slice_clone,
 } = Deno.core.ensureFastOps();
 
 // Cloning a buffer should result in the same buffer being returned
 test(function testBufferStore() {
-  const data = new Uint8Array(1024*1024);
+  const data = new Uint8Array(1024 * 1024);
   const buffer = data.buffer;
   op_v8slice_store("buffer", data);
   const output = op_v8slice_clone("buffer");
-  assertArrayEquals(output, new Uint8Array(1024*1024));
+  assertArrayEquals(output, new Uint8Array(1024 * 1024));
 });
 
 // Ensure that the returned buffer size is correct when a buffer is resized
 // externally via `ArrayBuffer.transfer`.
 test(function testBufferTransfer() {
-  const data = new Uint8Array(1024*1024);
+  const data = new Uint8Array(1024 * 1024);
   const buffer = data.buffer;
   op_v8slice_store("buffer", data);
   buffer.transfer(100);
