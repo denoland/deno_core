f4d93666a915e269226c195f6c9dc326bab93070
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-01-04 00:45:34 +0100

    add dprint plugin

--- wasm_example/import2.mjs
+++ wasm_example/import2.mjs
@@ -1,3 +1,8 @@
 import wasmMod from "./import.wasm" with { type: "wasm" };
 console.log(`hey ${wasmMod.exports.exported_add}`);
 console.log(`${wasmMod.exports.exported_add()}`);
+
+import dprintPlugin from "./plugin.wasm" with { type: "wasm" };
+console.log(`hey ${Object.keys(dprintPlugin.exports)}`);
+console.log("dprint plugin version", dprintPlugin.exports.get_plugin_schema_version());
+console.log("dprint plugin get_wasm_memory_buffer_size", dprintPlugin.exports.get_wasm_memory_buffer_size());
