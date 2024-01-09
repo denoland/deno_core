44039d82d73c96c1132b4812b5834de8279120c9
Matt Mastracci <matthew@mastracci.com>
2024-01-09 12:44:44 -0700

    .

--- testing/checkin/runner/ops_async.rs
+++ testing/checkin/runner/ops_async.rs
@@ -1,7 +1,31 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 use deno_core::op2;
+use deno_core::external;
+use deno_core::ExternalPointer;
 
 #[op2(async)]
 pub async fn op_async_void_deferred() {
   tokio::task::yield_now().await
+}
+
+struct Barrier(tokio::sync::Barrier);
+
+external!(Barrier, "barrier");
+
+#[op2(fast)]
+pub fn op_async_barrier_create(count: u32) -> *const std::ffi::c_void {
+  let barrier = Barrier(tokio::sync::Barrier::new(count as _));
+  ExternalPointer::new(barrier).into_raw()
+}
+
+#[op2(async)]
+pub async fn op_async_barrier_await(barrier: *const std::ffi::c_void) {
+  let barrier = ExternalPointer::<Barrier>::from_raw(barrier);
+  unsafe { barrier.unsafely_deref().0.wait().await; }
+}
+
+#[op2(fast)]
+pub fn op_async_barrier_destroy(barrier: *const std::ffi::c_void) {
+  let barrier = ExternalPointer::<Barrier>::from_raw(barrier);
+  unsafe { barrier.unsafely_take(); }
 }
--- testing/unit/ops_async_test.ts
+++ testing/unit/ops_async_test.ts
@@ -2,8 +2,22 @@
 import { test } from "checkin:testing";
 const {
   op_async_void_deferred,
+  op_async_barrier_create,
+  op_async_barrier_await,
+  op_async_barrier_destroy,
 } = Deno.core.ensureFastOps();
 
 test(async function testAsyncOp() {
   await op_async_void_deferred();
+});
+
+test(async function testAsyncBarrier() {
+  const count = 1e4;
+  let barrier = op_async_barrier_create(count);
+  let promises = [];
+  for (let i = 0; i < count; i++) {
+    promises.push(op_async_barrier_await(barrier));
+  }
+  await Promise.all(promises);
+  op_async_barrier_destroy(barrier);
 });
