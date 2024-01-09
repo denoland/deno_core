d451a4a36950a59d0a4eba7317a51b6566f28cb4
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-01-09 23:34:01 +0100

    remove unneeded file

--- core/examples/repl/repl_ops.js
+++
@@ -1,9 +0,0 @@
-const context = Deno.core.ops.repl_start();
-
-while (true) {
-  const input = Deno.core.ops.repl_read();
-  const output = Deno.core.ops.repl_evaluate(context, input);
-  console.log(output);
-}
-
-repl.context.Foobar = {...};
