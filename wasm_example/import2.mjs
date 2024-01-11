3d61bd926ef1a4d0665bca28c41f9fcd9463d816
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-01-11 22:20:15 +0100

    this is how it should look like

--- wasm_example/import2.mjs
+++ wasm_example/import2.mjs
@@ -1,8 +1,14 @@
-import wasmMod from "./import.wasm" with { type: "wasm" };
-console.log(`hey ${wasmMod.exports.exported_add}`);
-console.log(`${wasmMod.exports.exported_add()}`);
+import wasmModule from "./import.wasm" with { type: "wasm" };
+import bytes from "./import.wasm" with { type: "bytes" };
+console.log(`hey ${exported_add}`);
+console.log(`${exported_add()}`);
 
-import dprintPlugin from "./plugin.wasm" with { type: "wasm" };
-console.log(`hey ${Object.keys(dprintPlugin.exports)}`);
-console.log("dprint plugin version", dprintPlugin.exports.get_plugin_schema_version());
-console.log("dprint plugin get_wasm_memory_buffer_size", dprintPlugin.exports.get_wasm_memory_buffer_size());
+import {
+  get_plugin_schema_version,
+  get_wasm_memory_buffer_size,
+} from "./plugin.wasm" with { type: "wasm" };
+console.log("dprint plugin version", get_plugin_schema_version());
+console.log(
+  "dprint plugin get_wasm_memory_buffer_size",
+  get_wasm_memory_buffer_size(),
+);
