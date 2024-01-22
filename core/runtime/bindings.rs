cc7bd48d44fdc5bd2c80c07f302e895fd42c41fc
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-01-23 00:19:13 +0100

    Revert "revert"
    This reverts commit 589a88b9a4fcc58fbbf68f5d4ac3a077cb61889d.

--- core/runtime/bindings.rs
+++ core/runtime/bindings.rs
@@ -493,7 +493,7 @@ fn maybe_add_import_meta_filename_dirname(
   let dir_path = file_path
     .parent()
     .map(|p| p.to_owned())
-    .unwrap_or_else(|| PathBuf::from("/"));
+    .unwrap_or_else(|| file_path.ancestors().nth(0).unwrap().to_owned());
 
   let dirname_val =
     v8::String::new(scope, &dir_path.display().to_string()).unwrap();
