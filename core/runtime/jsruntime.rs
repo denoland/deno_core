16d927de7b2b119882a1277dd5516d3c78701d0d
digifox03 <alessandro@alecsn.it>
2024-01-24 09:16:07 +0100

    Rename feature flag to unsafe_use_unprotected_platform

--- core/runtime/jsruntime.rs
+++ core/runtime/jsruntime.rs
@@ -414,9 +414,8 @@ fn v8_init(
   v8::V8::set_flags_from_string(&flags);
 
   let v8_platform = v8_platform.unwrap_or_else(|| {
-    if cfg!(any(test, feature = "testing")) {
-      // We don't want to use the default platform in tests, because it
-      // uses memory protection keys which break multi-threaded tests.
+    if cfg!(any(test, feature = "unsafe_use_unprotected_platform")) {
+      // We want to use the unprotected platform for unit tests
       v8::new_unprotected_default_platform(0, false)
     } else {
       v8::new_default_platform(0, false)
