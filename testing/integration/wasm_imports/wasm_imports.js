04230cf3ab8a44ce82546b8fc5fe23d8e01eac12
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-02-05 16:30:55 +0100

    add instruction how to regenerate the file

--- testing/integration/wasm_imports/wasm_imports.js
+++ testing/integration/wasm_imports/wasm_imports.js
@@ -1,4 +1,7 @@
 // Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
 import { exported_add } from "./add.wasm";
 
+// To regenerate WASM file use:
+// npx -p wabt wat2wasm ./testing/integration/wasm_imports/add.wat -o ./testing/integration/wasm_imports/add.wasm
+
 console.log(`exported_add: ${exported_add(4, 5)}`);
