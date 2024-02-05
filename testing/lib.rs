8a034d75bdee0efa15ff71eec365c067f42e4dc9
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-02-05 16:44:08 +0100

    remove wasmer dep

--- testing/lib.rs
+++ testing/lib.rs
@@ -13,7 +13,6 @@ macro_rules! unit_test {
       $(
         #[test]
         fn $id() {
-          $crate::compile_wat_to_wasm();
           $crate::checkin::runner::run_unit_test(stringify!($id));
         }
       )*
@@ -27,28 +26,11 @@ macro_rules! integration_test {
       $(
         #[test]
         fn $id() {
-          $crate::compile_wat_to_wasm();
           $crate::checkin::runner::run_integration_test(stringify!($id));
         }
       )*
     }
   };
-}
-
-pub fn compile_wat_to_wasm() {
-  let wat_filename = PathBuf::from("./integration/wasm_imports/add.wat");
-  let contents = match std::fs::read(&wat_filename) {
-    Ok(c) => c,
-    Err(err) => panic!("Couldn't read {:?}, cause: {:#?}", wat_filename, err),
-  };
-  let wasm_bytes = match wasmer::wat2wasm(&contents) {
-    Ok(b) => b,
-    Err(err) => panic!("Couldn't compile wat to wasm, cause: {:#?}", err),
-  };
-  let wasm_filename = wat_filename.with_extension("wasm");
-  if let Err(err) = std::fs::write(&wasm_filename, wasm_bytes) {
-    panic!("Couldn't write {:?} file, cause: {:#?}", wasm_filename, err);
-  }
 }
 
 // Test individual bits of functionality. These files are loaded from the unit/ dir.
