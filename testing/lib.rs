0e39b08e10d72794009f470c95a6c62608644a09
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-02-05 03:22:54 +0100

    regenerate .wasm using wasmer

--- testing/lib.rs
+++ testing/lib.rs
@@ -3,6 +3,8 @@
 // Everything runs in test mode
 #![cfg(test)]
 
+use std::path::PathBuf;
+
 mod checkin;
 
 macro_rules! unit_test {
@@ -11,6 +13,7 @@ macro_rules! unit_test {
       $(
         #[test]
         fn $id() {
+          $crate::compile_wat_to_wasm();
           $crate::checkin::runner::run_unit_test(stringify!($id));
         }
       )*
@@ -24,11 +27,28 @@ macro_rules! integration_test {
       $(
         #[test]
         fn $id() {
+          $crate::compile_wat_to_wasm();
           $crate::checkin::runner::run_integration_test(stringify!($id));
         }
       )*
     }
   };
+}
+
+pub fn compile_wat_to_wasm() {
+  let wat_filename = PathBuf::from("./integration/wasm_imports/add.wat");
+  let contents = match std::fs::read(&wat_filename) {
+    Ok(c) => c,
+    Err(err) => panic!("Couldn't read {:?}, cause: {:#?}", wat_filename, err),
+  };
+  let wasm_bytes = match wasmer::wat2wasm(&contents) {
+    Ok(b) => b,
+    Err(err) => panic!("Couldn't compile wat to wasm, cause: {:#?}", err),
+  };
+  let wasm_filename = wat_filename.with_extension("wasm");
+  if let Err(err) = std::fs::write(&wasm_filename, wasm_bytes) {
+    panic!("Couldn't write {:?} file, cause: {:#?}", wasm_filename, err);
+  }
 }
 
 // Test individual bits of functionality. These files are loaded from the unit/ dir.
