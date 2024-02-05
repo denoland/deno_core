13120764ee33ecfdc2066a6dd1d5432932cc78d8
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-02-05 02:48:18 +0100

    remove big file

--- wasm_example/import.mjs
+++ wasm_example/import.mjs
@@ -1,10 +1,3 @@
 import { exported_add } from "./import.wasm";
 
 console.log(`exported_add: ${exported_add(4, 5)}`);
-
-import * as dprint from "./plugin.wasm";
-console.log("dprint plugin version:", dprint.get_plugin_schema_version());
-console.log(
-  "dprint plugin get_wasm_memory_buffer_size:",
-  dprint.get_wasm_memory_buffer_size(),
-);
