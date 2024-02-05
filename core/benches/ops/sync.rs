888ff954430893b051d4730d8fd1015d7a76e0af
Matt Mastracci <matthew@mastracci.com>
2024-02-05 14:59:48 -0700

    break fmt

--- core/benches/ops/sync.rs
+++ core/benches/ops/sync.rs
@@ -192,8 +192,7 @@ fn bench_op(
   // Prime the optimizer
   runtime
     .execute_script("", FastString::Owned(harness.into()))
-    .map_err(err_mapper)
-    .unwrap();
+    .map_err(err_mapper).unwrap();
   let bench = runtime.execute_script("", ascii_str!("bench")).unwrap();
   let mut scope = runtime.handle_scope();
   #[allow(clippy::unnecessary_fallible_conversions)]
