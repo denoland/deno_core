08722b0b44f46bbb99cf357e2a208326411c3fb2
Jonas Platte <158304798+svix-jplatte@users.noreply.github.com>
2024-03-08 20:11:36 +0100

    Replace usage of derive_more derives with manual implementations (#650)
    I found some of the generated impls like [this
    one](https://docs.rs/serde_v8/latest/serde_v8/struct.BigInt.html#impl-AsMut%3C__AsMutT%3E-for-BigInt)
    quite weird so I removed or replaced them with different ones, as seemed
    reasonable in the specific cases.
    
    Let me know what you think. The individual commit messages have more
    details on why I chose certain replacements.
    
    Closes #640.

--- serde_v8/magic/u16string.rs
+++ serde_v8/magic/u16string.rs
@@ -1,16 +1,67 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
+use std::ops::Deref;
+use std::ops::DerefMut;
+
 use crate::Error;
 
 use super::transl8::impl_magic;
-use super::transl8::impl_wrapper;
 use super::transl8::FromV8;
 use super::transl8::ToV8;
 
-impl_wrapper!(
-  pub struct U16String(Vec<u16>);
-);
+#[derive(PartialEq, Eq, Clone, Debug, Default)]
+pub struct U16String(Vec<u16>);
 impl_magic!(U16String);
+
+impl Deref for U16String {
+  type Target = Vec<u16>;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+
+impl DerefMut for U16String {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    &mut self.0
+  }
+}
+
+impl AsRef<[u16]> for U16String {
+  fn as_ref(&self) -> &[u16] {
+    &self.0
+  }
+}
+
+impl AsMut<[u16]> for U16String {
+  fn as_mut(&mut self) -> &mut [u16] {
+    &mut self.0
+  }
+}
+
+impl<const N: usize> From<[u16; N]> for U16String {
+  fn from(value: [u16; N]) -> Self {
+    Self(value.into())
+  }
+}
+
+impl<const N: usize> From<&[u16; N]> for U16String {
+  fn from(value: &[u16; N]) -> Self {
+    Self(value.into())
+  }
+}
+
+impl From<&[u16]> for U16String {
+  fn from(value: &[u16]) -> Self {
+    Self(value.into())
+  }
+}
+
+impl From<Vec<u16>> for U16String {
+  fn from(value: Vec<u16>) -> Self {
+    Self(value)
+  }
+}
 
 impl ToV8 for U16String {
   fn to_v8<'a>(
