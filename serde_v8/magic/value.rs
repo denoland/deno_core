e27eff06191eb18f69a99c6ace2b7bdf628833a9
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-04-01 08:02:33 +0100

    refactor: Make serde_v8::Value private (#681)
    This commit makes serde_v8::Value variant private - meaning that users
    can no longer accept `serde_v8::Value` in the ops as arguments. Instead
    `v8::Value` should be used directly. It is also disallowed to use
    `serde_v8::Value` in the structure arguments - instead the 
    `v8::Value` should be passed as an explicit argument. It lowers 
    flexibility, but makes it so that there's only a single way of passing 
    V8 values.
    
    Unfortunately the implementation itself can't be removed because it's
    used internally to provide serde for `#[serde]` arguments.

--- serde_v8/magic/value.rs
+++ serde_v8/magic/value.rs
@@ -4,42 +4,18 @@ use crate::magic::transl8::FromV8;
 use crate::magic::transl8::ToV8;
 use std::mem::transmute;
 
-/// serde_v8::Value allows passing through `v8::Value`s untouched
-/// when de/serializing & allows mixing rust & v8 values in structs, tuples...
+/// serde_v8::Value is used internally to serialize/deserialize values in
+/// objects and arrays. This struct was exposed to user code in the past, but
+/// we don't want to do that anymore as it leads to inefficient usages - eg. wrapping
+/// a V8 object in `serde_v8::Value` and then immediately unwrapping it.
 //
 // SAFETY: caveat emptor, the rust-compiler can no longer link lifetimes to their
-// original scope, you must take special care in ensuring your handles don't outlive their scope
+// original scope, you must take special care in ensuring your handles don't
+// outlive their scope.
 pub struct Value<'s> {
   pub v8_value: v8::Local<'s, v8::Value>,
 }
 impl_magic!(Value<'_>);
-
-impl<'s> Value<'s> {
-  /// Converts this to a [`v8::Global`] infallibly, if this `v8` type can be converted infallibly
-  /// to [`v8::Global<T>`] (eg: [`v8::Data`] or [`v8::Value`]).
-  pub fn as_global<T>(&self, scope: &mut v8::HandleScope<'s>) -> v8::Global<T>
-  where
-    v8::Local<'s, T>: From<v8::Local<'s, v8::Value>>,
-  {
-    let local = v8::Local::<T>::from(self.v8_value);
-    v8::Global::new(scope, local)
-  }
-
-  /// Converts this to a [`v8::Global`] fallibly.
-  pub fn try_as_global<T>(
-    &self,
-    scope: &mut v8::HandleScope<'s>,
-  ) -> Result<
-    v8::Global<T>,
-    <v8::Local<'s, T> as TryFrom<v8::Local<'s, v8::Value>>>::Error,
-  >
-  where
-    v8::Local<'s, T>: TryFrom<v8::Local<'s, v8::Value>>,
-  {
-    let local = v8::Local::<T>::try_from(self.v8_value)?;
-    Ok(v8::Global::new(scope, local))
-  }
-}
 
 impl<'s, T> From<v8::Local<'s, T>> for Value<'s>
 where
@@ -73,94 +49,5 @@ impl FromV8 for Value<'_> {
   ) -> Result<Self, crate::Error> {
     // SAFETY: not fully safe, since lifetimes are detached from original scope
     Ok(unsafe { transmute::<Value, Value>(value.into()) })
-  }
-}
-
-macro_rules! impl_try_from {
-  ($($type:ident),+) => {$(
-    /// Implements `TryFrom` for the given `v8` type. If the type is not compatible,
-    /// returns `Err(v8::DataError)`.
-    impl<'s> TryFrom<Value<'s>> for v8::Local<'s, v8::$type> {
-      type Error = v8::DataError;
-      fn try_from(value: Value<'s>) -> Result<Self, Self::Error> {
-        value.try_into()
-      }
-    }
-  )+};
-}
-
-impl_try_from!(
-  External,
-  Object,
-  Array,
-  ArrayBuffer,
-  ArrayBufferView,
-  DataView,
-  TypedArray,
-  BigInt64Array,
-  BigUint64Array,
-  Float32Array,
-  Float64Array,
-  Int16Array,
-  Int32Array,
-  Int8Array,
-  Uint16Array,
-  Uint32Array,
-  Uint8Array,
-  Uint8ClampedArray,
-  BigIntObject,
-  BooleanObject,
-  Date,
-  Function,
-  Map,
-  NumberObject,
-  Promise,
-  PromiseResolver,
-  Proxy,
-  RegExp,
-  Set,
-  SharedArrayBuffer,
-  StringObject,
-  SymbolObject,
-  WasmMemoryObject,
-  WasmModuleObject,
-  Primitive,
-  BigInt,
-  Boolean,
-  Name,
-  String,
-  Symbol,
-  Number,
-  Integer,
-  Int32,
-  Uint32
-);
-
-#[cfg(test)]
-mod tests {
-  use std::convert::Infallible;
-
-  #[test]
-  #[ignore]
-  fn test_conversions_compile() {
-    // SAFETY: We don't run this test -- it's just to make sure this code compiles
-    #[allow(invalid_value)]
-    #[allow(clippy::unnecessary_fallible_conversions)]
-    unsafe {
-      let value: v8::Local<v8::Function> = std::mem::zeroed();
-
-      // TryInto compiles for non-value
-      let serde_value: super::Value = value.into();
-      let _: Result<v8::Local<v8::Function>, v8::DataError> =
-        serde_value.try_into();
-
-      // TryInto compiles for value
-      let serde_value: super::Value = value.into();
-      let _: Result<v8::Local<v8::Value>, Infallible> = serde_value.try_into();
-
-      // Into compiles for value
-      let serde_value: super::Value = value.into();
-      let _: v8::Local<v8::Value> = serde_value.into();
-    }
   }
 }
