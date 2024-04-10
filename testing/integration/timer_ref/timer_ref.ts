48fff9f8be9e2eff55be951b1993a2a842e19081
Matt Mastracci <matthew@mastracci.com>
2024-04-10 13:58:33 -0600

    .

--- testing/integration/timer_ref/timer_ref.ts
+++ testing/integration/timer_ref/timer_ref.ts
@@ -1,5 +1,5 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 setTimeout(() => console.log("a"), 1000);
 setTimeout(() => console.log("b"), 2000);
-const c = setTimeout(() => console.log("c"), 3000);
+const c = setTimeout(() => console.log("c"), 30_000);
 Deno.core.unrefTimer(c);
