64b35fa10747266150dab5a2324ee0657f931880
Matt Mastracci <matthew@mastracci.com>
2024-04-11 11:51:55 -0600

    chore: add some code that was crashing deno (#695)

---
+++ testing/integration/ts_types/ts_types.ts
@@ -0,0 +1,5 @@
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+import * as which from "./which.ts";
+import * as decorator from "./decorator.ts";
+console.log(which);
+console.log(decorator);
