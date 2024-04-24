d640bf8cf4d3028fb4aaabb27dddf39138d724c3
Nathan Whitaker <nathan@deno.com>
2024-04-24 17:22:09 -0400

    Send + sync + 'static error / no option impl

--- core/to_from_v8.rs
+++ core/to_from_v8.rs
@@ -1,7 +1,7 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
 pub trait ToV8<'a> {
-  type Error: std::error::Error;
+  type Error: std::error::Error + Send + Sync + 'static;
 
   fn to_v8(
     self,
@@ -10,47 +10,10 @@ pub trait ToV8<'a> {
 }
 
 pub trait FromV8<'a>: Sized {
-  type Error: std::error::Error;
+  type Error: std::error::Error + Send + Sync + 'static;
 
   fn from_v8(
     scope: &mut v8::HandleScope<'a>,
     value: v8::Local<'a, v8::Value>,
   ) -> Result<Self, Self::Error>;
-}
-
-// Impls for Option
-
-impl<'a, T> ToV8<'a> for Option<T>
-where
-  T: ToV8<'a>,
-{
-  type Error = T::Error;
-
-  fn to_v8(
-    self,
-    scope: &mut v8::HandleScope<'a>,
-  ) -> Result<v8::Local<'a, v8::Value>, Self::Error> {
-    match self {
-      Some(x) => x.to_v8(scope).map(Into::into),
-      None => Ok(v8::undefined(scope).into()),
-    }
-  }
-}
-
-impl<'a, T> FromV8<'a> for Option<T>
-where
-  T: FromV8<'a>,
-{
-  type Error = T::Error;
-
-  fn from_v8(
-    scope: &mut v8::HandleScope<'a>,
-    value: v8::Local<'a, v8::Value>,
-  ) -> Result<Self, Self::Error> {
-    if value.is_null_or_undefined() {
-      Ok(None)
-    } else {
-      T::from_v8(scope, value).map(Some)
-    }
-  }
 }
