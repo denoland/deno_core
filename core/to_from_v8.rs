faad171cda952691c86b666c1e64dd4b7edc4579
Nathan Whitaker <nathan@deno.com>
2024-05-02 15:35:39 -0700

    Fallible FromV8 for Smi/Number

--- core/to_from_v8.rs
+++ core/to_from_v8.rs
@@ -1,6 +1,6 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
-use crate::runtime::ops;
+use crate::{error::StdAnyError, runtime::ops};
 use std::convert::Infallible;
 
 pub trait ToV8<'a> {
@@ -28,6 +28,7 @@ pub trait FromV8<'a>: Sized {
 pub struct Smi<T: SmallInt>(pub T);
 
 pub trait SmallInt {
+  const NAME: &'static str;
   fn as_i32(self) -> i32;
   fn from_i32(value: i32) -> Self;
 }
@@ -36,6 +37,7 @@ macro_rules! impl_smallint {
     (for $($t:ty),*) => {
         $(
             impl SmallInt for $t {
+                const NAME: &'static str = stringify!($t);
                 #[inline(always)]
                 fn as_i32(self) -> i32 {
                   self as _
@@ -55,6 +57,7 @@ impl_smallint!(for u8, u16, u32, u64, usize, i8, i16, i32, i64, isize);
 impl<'a, T: SmallInt> ToV8<'a> for Smi<T> {
   type Error = Infallible;
 
+  #[inline]
   fn to_v8(
     self,
     scope: &mut v8::HandleScope<'a>,
@@ -64,13 +67,15 @@ impl<'a, T: SmallInt> ToV8<'a> for Smi<T> {
 }
 
 impl<'a, T: SmallInt> FromV8<'a> for Smi<T> {
-  type Error = Infallible;
+  type Error = StdAnyError;
 
+  #[inline]
   fn from_v8(
     _scope: &mut v8::HandleScope<'a>,
     value: v8::Local<'a, v8::Value>,
   ) -> Result<Self, Self::Error> {
-    let v = crate::runtime::ops::to_i32_option(&value).unwrap_or_default();
+    let v = crate::runtime::ops::to_i32_option(&value)
+      .ok_or_else(|| crate::error::type_error(format!("{}", T::NAME)))?;
     Ok(Smi(T::from_i32(v)))
   }
 }
@@ -78,7 +83,8 @@ impl<'a, T: SmallInt> FromV8<'a> for Smi<T> {
 pub struct Number<T: Numeric>(pub T);
 
 pub trait Numeric: Sized {
-  fn from_value(value: &v8::Value) -> Self;
+  const NAME: &'static str;
+  fn from_value(value: &v8::Value) -> Option<Self>;
   fn as_f64(self) -> f64;
 }
 
@@ -86,9 +92,10 @@ macro_rules! impl_numeric {
   ($($t:ty : $from: path ),*) => {
       $(
           impl Numeric for $t {
+              const NAME: &'static str = stringify!($t);
               #[inline(always)]
-              fn from_value(value: &v8::Value) -> Self {
-                $from(value).unwrap_or_default() as _
+              fn from_value(value: &v8::Value) -> Option<Self> {
+                $from(value).map(|v| v as _)
               }
 
               #[inline(always)]
@@ -123,13 +130,15 @@ impl<'a, T: Numeric> ToV8<'a> for Number<T> {
 }
 
 impl<'a, T: Numeric> FromV8<'a> for Number<T> {
-  type Error = Infallible;
+  type Error = StdAnyError;
   #[inline]
   fn from_v8(
     _scope: &mut v8::HandleScope<'a>,
     value: v8::Local<'a, v8::Value>,
   ) -> Result<Self, Self::Error> {
-    Ok(Number(T::from_value(&value)))
+    T::from_value(&value).map(Number).ok_or_else(|| {
+      crate::error::type_error(format!("Expected {}", T::NAME)).into()
+    })
   }
 }
 
