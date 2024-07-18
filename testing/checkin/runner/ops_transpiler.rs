cc573b4c99aa53bbbde9dc1292d709bcb419d5f1
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-07-18 02:22:51 +0200

    wip upgrade

--- testing/checkin/runner/ops_transpiler.rs
+++ testing/checkin/runner/ops_transpiler.rs
@@ -3,7 +3,6 @@
 use deno_ast::MediaType;
 use deno_ast::ParseParams;
 use deno_ast::SourceMapOption;
-use deno_ast::SourceTextInfo;
 use deno_core::error::AnyError;
 use deno_core::op2;
 use deno_core::url::Url;
@@ -18,19 +17,30 @@ pub fn op_transpile(
 
   let parsed = deno_ast::parse_module(ParseParams {
     specifier: Url::parse(specifier).unwrap(),
-    text_info: SourceTextInfo::from_string(source),
+    text: source.into(),
     media_type,
     capture_tokens: false,
     scope_analysis: false,
     maybe_syntax: None,
   })?;
-  let transpiled_source = parsed.transpile(&deno_ast::EmitOptions {
-    imports_not_used_as_values: deno_ast::ImportsNotUsedAsValues::Remove,
-    source_map: SourceMapOption::Inline,
-    inline_sources: false,
-    use_decorators_proposal: true,
-    ..Default::default()
-  })?;
+  let transpile_result = parsed.transpile(
+    &deno_ast::TranspileOptions {
+      use_decorators_proposal: true,
+      imports_not_used_as_values: deno_ast::ImportsNotUsedAsValues::Remove,
+      ..Default::default()
+    },
+    &deno_ast::EmitOptions {
+      source_map: SourceMapOption::Inline,
+      inline_sources: false,
+      ..Default::default()
+    },
+  )?;
+  let transpiled_source = transpile_result.into_source();
 
-  Ok((transpiled_source.text, transpiled_source.source_map))
+  Ok((
+    String::from_utf8(transpiled_source.source).unwrap(),
+    transpiled_source
+      .source_map
+      .map(|s| String::from_utf8(s).unwrap()),
+  ))
 }
