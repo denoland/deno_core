3523bc118886001efbdeaf4ba281c4139a67b8cf
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-08-31 01:07:44 +0200

    feat: JsRuntimeInspector::ensure_default_context_in_group

--- core/runtime/jsruntime.rs
+++ core/runtime/jsruntime.rs
@@ -1020,7 +1020,12 @@ impl JsRuntime {
     }
 
     let inspector = if options.inspector {
-      Some(JsRuntimeInspector::new(scope, context, options.is_main))
+      Some(JsRuntimeInspector::new(
+        isolate_ptr,
+        scope,
+        context,
+        options.is_main,
+      ))
     } else {
       None
     };
@@ -1684,6 +1689,7 @@ impl JsRuntime {
     }
 
     let context = self.main_context();
+    let isolate_ptr = self.inner.v8_isolate.as_mut() as *mut _;
     let scope = &mut v8::HandleScope::with_context(
       self.inner.v8_isolate.as_mut(),
       context.clone(),
@@ -1692,6 +1698,7 @@ impl JsRuntime {
 
     self.inner.state.has_inspector.set(true);
     **inspector = Some(JsRuntimeInspector::new(
+      isolate_ptr,
       scope,
       context,
       self.is_main_runtime,
