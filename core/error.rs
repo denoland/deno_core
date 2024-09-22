e47774120ad19bac988ad84c56531c20739d2fe6
Divy Srivastava <dj.srivastava23@gmail.com>
2024-09-22 10:06:55 +0530

    Fix panic in `prepare_stack_trace_callback` when global interceptor throws
    Fix panic when a global interceptor is misconfigured or throws an
    exception. Example
    https://github.com/denoland/deno/pull/24985#issuecomment-2365460210

--- core/error.rs
+++ core/error.rs
@@ -1250,9 +1250,13 @@ pub fn prepare_stack_trace_callback<'s>(
 
   // `globalThis.Error.prepareStackTrace`
   let global = scope.get_current_context().global(scope);
-  let global_error = get_property(scope, global, ERROR).unwrap().cast();
-  let prepare_fn = get_property(scope, global_error, PREPARE_STACK_TRACE)
-    .and_then(|f| f.try_cast::<v8::Function>().ok());
+  let global_error = get_property(scope, global, ERROR).map(|g| g.cast());
+  let prepare_fn = global_error
+    .map(|g| {
+      get_property(scope, g, PREPARE_STACK_TRACE)
+        .and_then(|f| f.try_cast::<v8::Function>().ok())
+    })
+    .flatten();
 
   if let Some(prepare_fn) = prepare_fn {
     // User defined `Error.prepareStackTrace`.
@@ -1274,7 +1278,7 @@ pub fn prepare_stack_trace_callback<'s>(
     let patched_callsites = v8::Array::new_with_elements(scope, &patched);
 
     // call the user's `prepareStackTrace` with our patched "callsite" objects
-    let this = global_error.into();
+    let this = global_error.unwrap().into();
     let args = &[error, patched_callsites.into()];
     return prepare_fn
       .call(scope, this, args)
