f217fe0a6730bfef2a7f2a75fa32659d71d80a95
snek <snek@deno.com>
2024-10-18 12:21:59 +0200

    fix: handle module states in op_import_sync (#935)
    Handle all the various states a module could be in when running
    `op_import_sync`.
    
    Refs: https://github.com/denoland/deno/issues/26124

--- testing/lib.rs
+++ testing/lib.rs
@@ -71,6 +71,7 @@ integration_test!(
   error_get_file_name_to_string,
   error_get_script_name_or_source_url,
   import_sync,
+  import_sync_existing,
   main_module_handler,
   module_types,
   pending_unref_op_tla,
