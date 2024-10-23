09219cad9a65661a78ea8bc77f7c2b9924e18c3c
snek <snek@deno.com>
2024-10-23 11:51:32 +0200

    refactor: inspector changes for deno (#938)
    changes for https://github.com/denoland/deno/pull/26471
    
    - allows passing a function which is called directly from the inspector
    to send messages, rather than going through a channel.
    - new modes for inspector sessions to support all the various ways a
    session interacts with the event loop

--- core/lib.rs
+++ core/lib.rs
@@ -86,6 +86,8 @@ pub use crate::feature_checker::FeatureChecker;
 pub use crate::flags::v8_set_flags;
 pub use crate::inspector::InspectorMsg;
 pub use crate::inspector::InspectorMsgKind;
+pub use crate::inspector::InspectorSessionKind;
+pub use crate::inspector::InspectorSessionOptions;
 pub use crate::inspector::InspectorSessionProxy;
 pub use crate::inspector::JsRuntimeInspector;
 pub use crate::inspector::LocalInspectorSession;
