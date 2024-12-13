37396b3454df7d765289835673decb9a01c12442
Divy Srivastava <dj.srivastava23@gmail.com>
2024-12-13 20:52:46 +0530

    fix

--- ops/op2/test_cases/async/async_cppgc.rs
+++ ops/op2/test_cases/async/async_cppgc.rs
@@ -1,11 +1,14 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 #![deny(warnings)]
 deno_ops_compile_test_runner::prelude!();
+use deno_core::cppgc::Identifier;
 use deno_core::GarbageCollected;
 
 struct Wrap;
 
 impl GarbageCollected for Wrap {}
+
+impl Identifier for Wrap {}
 
 #[op2(async)]
 #[cppgc]
--- ops/op2/test_cases_fail/lifetimes.rs
+++ ops/op2/test_cases_fail/lifetimes.rs
@@ -1,6 +1,7 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 #![deny(warnings)]
 deno_ops_compile_test_runner::prelude!();
+use deno_core::cppgc::Identifier;
 use deno_core::error::AnyError;
 use deno_core::GarbageCollected;
 use std::future::Future;
@@ -8,6 +9,8 @@ use std::future::Future;
 struct Wrap;
 
 impl GarbageCollected for Wrap {}
+
+impl Identifier for Wrap {}
 
 #[op2(fast)]
 fn op_use_cppgc_object(#[cppgc] _wrap: &'static Wrap) {}
