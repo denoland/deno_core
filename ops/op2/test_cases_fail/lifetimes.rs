77c65eda54eede17223410996fd11eaf9a855d73
Divy Srivastava <dj.srivastava23@gmail.com>
2024-12-14 09:19:38 +0530

    x

--- ops/op2/test_cases_fail/lifetimes.rs
+++ ops/op2/test_cases_fail/lifetimes.rs
@@ -1,7 +1,6 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 #![deny(warnings)]
 deno_ops_compile_test_runner::prelude!();
-use deno_core::cppgc::Identifier;
 use deno_core::error::AnyError;
 use deno_core::GarbageCollected;
 use std::future::Future;
