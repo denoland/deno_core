95eb1420fe4b1e586dd8bac94db6cf2aceab8d25
Tad Hardesty <tad@dcspark.io>
2025-01-02 15:37:49 -0800

    Fix ModuleTypes when creating text and binary modules

--- core/modules/map.rs
+++ core/modules/map.rs
@@ -857,7 +857,7 @@ impl ModuleMap {
     let source_str_local = v8::Local::new(scope, source_str);
     let source_value_local = v8::Local::<v8::Value>::from(source_str_local);
     let exports = vec![(ascii_str!("default"), source_value_local)];
-    self.new_synthetic_module(scope, name, ModuleType::Json, exports)
+    self.new_synthetic_module(scope, name, ModuleType::Text, exports)
   }
 
   pub(crate) fn new_binary_module(
@@ -876,7 +876,7 @@ impl ModuleMap {
     let source_arraybuffer = v8::ArrayBuffer::with_backing_store(scope, &backing_store.make_shared());
     let source_value_local = v8::Local::<v8::Value>::from(source_arraybuffer);
     let exports = vec![(ascii_str!("default"), source_value_local)];
-    self.new_synthetic_module(scope, name, ModuleType::Json, exports)
+    self.new_synthetic_module(scope, name, ModuleType::Binary, exports)
   }
 
   pub(crate) fn instantiate_module(
