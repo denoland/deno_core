fb8ce24d983b6e6e8b8e41c856a49883d9adec86
Leo Kettmeir <crowlkats@toaxl.com>
2025-01-09 15:08:11 +0100

    refactor: simplify macro testing infrastructure (#1040)

--- ops/lib.rs
+++ ops/lib.rs
@@ -42,3 +42,57 @@ pub fn webidl(item: TokenStream) -> TokenStream {
     Err(err) => err.into_compile_error().into(),
   }
 }
+
+#[cfg(test)]
+mod infra {
+  use std::path::PathBuf;
+  use syn::File;
+
+  pub fn run_macro_expansion_test<F, I>(input: PathBuf, expander: F)
+  where
+    F: FnOnce(File) -> I,
+    I: Iterator<Item = proc_macro2::TokenStream>,
+  {
+    let update_expected = std::env::var("UPDATE_EXPECTED").is_ok();
+
+    let source =
+      std::fs::read_to_string(&input).expect("Failed to read test file");
+
+    const PRELUDE: &str = r"// Copyright 2018-2025 the Deno authors. MIT license.
+
+#![deny(warnings)]
+deno_ops_compile_test_runner::prelude!();";
+
+    if !source.starts_with(PRELUDE) {
+      panic!("Source does not start with expected prelude:]n{PRELUDE}");
+    }
+
+    let file =
+      syn::parse_str::<File>(&source).expect("Failed to parse Rust file");
+
+    let expected_out = expander(file)
+      .map(|tokens| {
+        println!("======== Raw tokens ========:\n{}", tokens.clone());
+        let tree = syn::parse2(tokens).unwrap();
+        let actual = prettyplease::unparse(&tree);
+        println!("======== Generated ========:\n{}", actual);
+        actual
+      })
+      .collect::<Vec<String>>()
+      .join("\n");
+
+    if update_expected {
+      std::fs::write(input.with_extension("out"), expected_out)
+        .expect("Failed to write expectation file");
+    } else {
+      let expected = std::fs::read_to_string(input.with_extension("out"))
+        .expect("Failed to read expectation file");
+
+      pretty_assertions::assert_eq!(
+        expected,
+        expected_out,
+        "Failed to match expectation. Use UPDATE_EXPECTED=1."
+      );
+    }
+  }
+}
