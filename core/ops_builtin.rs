ec8daa5b788b023e3dc473c99bd992db53743514
Jasper van Esveld <jaspervanesveld@gmail.com>
2025-04-10 04:25:07 +0200

    Format changes

--- core/ops_builtin.rs
+++ core/ops_builtin.rs
@@ -465,10 +465,7 @@ fn op_encode_binary_string(#[buffer] s: &[u8]) -> ByteString {
 }
 
 #[op2(fast)]
-fn op_is_terminal(
-  state: &mut OpState,
-  #[smi] rid: ResourceId,
-) -> bool {
+fn op_is_terminal(state: &mut OpState, #[smi] rid: ResourceId) -> bool {
   match state.resource_table.get_handle(rid) {
     Ok(handle) => handle.is_terminal(),
     _ => false,
