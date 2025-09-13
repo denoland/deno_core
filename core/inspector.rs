63075813f7a5e5c36ce3d138b74f27f705267a2b
Bartek Iwańczuk <biwanczuk@gmail.com>
2025-09-13 11:43:47 +0200

    a

--- core/inspector.rs
+++ core/inspector.rs
@@ -423,16 +423,14 @@ impl JsRuntimeInspector {
   /// established a websocket connection.
   pub fn wait_for_session(&mut self) {
     loop {
-      match self.sessions.get_mut().established.iter_mut().next() {
-        Some(_session) => {
-          self.flags.get_mut().waiting_for_session = false;
-          break;
-        }
-        None => {
-          self.flags.get_mut().waiting_for_session = true;
-          let _ = self.poll_sessions(None).unwrap();
-        }
-      };
+      // TODO: doesn't account for "local" sessions
+      if self.sessions.get_mut().established.is_empty() {
+        self.flags.get_mut().waiting_for_session = true;
+        let _ = self.poll_sessions(None).unwrap();
+      } else {
+        self.flags.get_mut().waiting_for_session = false;
+        break;
+      }
     }
   }
 
