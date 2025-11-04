1d77a437799139920f85ce43166a58456a69ccb7
Bartek Iwańczuk <biwanczuk@gmail.com>
2025-11-04 16:25:11 +0100

    feat(BREAKING): remove support for `op2` `#[arraybuffer]` attribute

--- ops/op2/test_cases/sync/buffers_out.rs
+++ ops/op2/test_cases/sync/buffers_out.rs
@@ -16,17 +16,3 @@ fn op_buffers(#[buffer] buffer: JsBuffer) -> JsBuffer {
 fn op_buffers_option(#[buffer] buffer: Option<JsBuffer>) -> Option<JsBuffer> {
   buffer
 }
-
-#[op2]
-#[arraybuffer]
-fn op_arraybuffers(#[arraybuffer] buffer: JsBuffer) -> JsBuffer {
-  buffer
-}
-
-// TODO(mmastrac): Option + Marker doesn't work yet
-
-// #[op2]
-// #[arraybuffer]
-// fn op_arraybuffers_option(#[arraybuffer] buffer: Option<JsBuffer>) -> Option<JsBuffer> {
-//   buffer
-// }
