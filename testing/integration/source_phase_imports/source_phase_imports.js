866a7cfdffde554a089eb3b77a6e5ff62fdc6688
Nayeem Rahman <nayeemrmn99@gmail.com>
2025-11-20 02:22:05 +0000

    fixup! syntax error tests

--- testing/integration/source_phase_imports/source_phase_imports.js
+++ testing/integration/source_phase_imports/source_phase_imports.js
@@ -1,3 +1,4 @@
+// Copyright 2018-2025 the Deno authors. MIT license.
 import source foo from "../wasm_imports/add.wasm";
 console.log(foo);
 import source bar from "./other.js";
--- testing/integration/source_phase_imports_dynamic/source_phase_imports_dynamic.js
+++ testing/integration/source_phase_imports_dynamic/source_phase_imports_dynamic.js
@@ -1,3 +1,4 @@
+// Copyright 2018-2025 the Deno authors. MIT license.
 const foo = await import.source("../wasm_imports/add.wasm");
 console.log(foo instanceof WebAssembly.Module);
 const bar = await import.source("./other.js");
