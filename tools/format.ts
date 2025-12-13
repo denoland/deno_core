94462ab890793e844b7a29806afa8876550def46
Asher Gomez <ashersaupingomez@gmail.com>
2025-12-14 10:31:16 +1100

    chore: use `deno fmt`

--- tools/format.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S deno run --quiet -RWE --allow-run
-// Copyright 2018-2025 the Deno authors. MIT license.
-import { $ } from "@deno/rust-automation";
-
-if (Deno.args[0] == "--check") {
-  await $`deno run -A npm:dprint@0.47.6 check`;
-} else {
-  await $`deno run -A npm:dprint@0.47.6 fmt`;
-}
