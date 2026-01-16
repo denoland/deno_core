add9b9c63064e973be3fb632cc26476d3ca2ee1d
Divy <dj.srivastava23@gmail.com>
2026-01-16 22:16:09 +0530

    Fix reset sleep state when timer set becomes empty (#1277)

--- core/web_timeout.rs
+++ core/web_timeout.rs
@@ -205,6 +205,7 @@ impl MutableSleep {
     unsafe {
       *self.sleep.get() = None;
     }
+    self.ready.set(false);
   }
 
   fn change(self: &Box<Self>, instant: Instant) {
@@ -466,8 +467,8 @@ impl<T: Clone> WebTimers<T> {
       // When the # of running timers hits zero, clear the timer tree.
       if !timers.is_empty() {
         timers.clear();
-        self.sleep.clear();
       }
+      self.sleep.clear();
     } else {
       // Run compaction when there are enough tombstones to justify cleanup.
       let tombstone_count = timers.len() - data.len();
